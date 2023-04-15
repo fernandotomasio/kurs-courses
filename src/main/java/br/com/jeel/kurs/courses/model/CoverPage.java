c850b219221a613914105599c589356ad832f5b2
Fernando Tomasio <fernando.tomasio@sys4b.com.br>
2023-04-15 18:57:02 -0300

    fix: update cover page attrs

--- src/main/java/br/com/jeel/kurs/courses/model/CoverPage.java
+++ src/main/java/br/com/jeel/kurs/courses/model/CoverPage.java
@@ -1,17 +1,25 @@
 package br.com.jeel.kurs.courses.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+/**
+ *
+ * @author fernandofot
+ */
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CoverPage {
-    private String title;
-    private String description;
-    private String author;
-    private String date;
-    private String version;
-    private String logoUrl;
+
+    private String objective;
+    private String level;
+    private String prerequisites;
+    private String disciplines;
+    private String maintenanceLevel;
+    private String qualification;
+    private String notes;
 }
