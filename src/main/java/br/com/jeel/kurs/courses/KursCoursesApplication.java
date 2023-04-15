4905dd18e50a8d31b91acd4260729796578d457c
Fernando Tomasio <fernando.tomasio@sys4b.com.br>
2023-04-15 13:28:31 -0300

    first commit

---
+++ src/main/java/br/com/jeel/kurs/courses/KursCoursesApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeel.kurs.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeel/kurs/courses/KursCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeel.kurs.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
